d5a744f042b897b19088a7516ac6f4013379891b
laurenc176 <60895959+laurenc176@users.noreply.github.com>
2020-02-18 13:22:54 -0500

    Add files via upload

---
+++ simple_lists_cars.py
@@ -0,0 +1,34 @@
+#organizing a list
+
+#sorting a list permanently with the sort() method, can never revert 
+#to original order
+cars = ['bmw','audi','toyota','subaru']
+cars.sort()
+print(cars)
+
+#can also do reverse
+cars.sort(reverse=True)
+print(cars)
+
+# sorted() function sorts  a list temporarily
+cars = ['bmw','audi','toyota','subaru']
+print("Here is the orginal list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+# Can print a list in reverse order using reverse() method, changes it permanently 
+# doesnt sort backward alphabetically, simply reverses order of the list
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+#other methods I already know:
+len(cars)
+
