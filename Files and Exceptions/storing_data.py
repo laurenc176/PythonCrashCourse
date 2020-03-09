d5fd7f8311beea5f47c8eb5d3bd8072d0b7addfd
laurenc176 <60895959+laurenc176@users.noreply.github.com>
2020-03-09 17:29:50 -0400

    Update storing_data.py

--- Files and Exceptions/storing_data.py
+++ Files and Exceptions/storing_data.py
@@ -47,6 +47,79 @@ else:
 #functions that have specific jobs.  Makes code cleaner, easier to understand, and easier to extend.
 
 #Refactor remember me from above:
+import json
+def greet_user():
+	"""Greet user by name"""
+	filename = 'username.json'
+	try:
+		with open(filename) as f:
+			username = json.load(f)
+	except FileNotFoundError:
+		username = input("What is your name? ")
+		with open(filename, 'w') as f:
+			json.dump(username, f)
+			print(f"We'll remember you when you come back, {username}!")
+	else:
+		print(f"Welcome back, {username}!")
+
+---------------------------------------------------------------
+#Refactor greet_user()
+import jason
+
+def get_stored_username():
+	"""Get stored username if available"""
+	filename = 'username.json'
+	try:
+		with open(filename) as f:
+			username = json.load(f)
+	except FileNotFoundError:
+		return None 
+	else:
+		return username
+
+def greet_user():
+	"""Greet the user by name"""
+	username = get_stored_username()
+	if username:
+		print(f"Welcome back, {username}")
+	else:
+		username = input("What is your name? ")
+		filename = 'username.json'
+		with open (filename, 'w') as f:
+			json.dump(username, f)
+			print(f"We'll remember you when you come back, {username}.")
+
+----------------------------------------------------------
+#Refactor again:
+
+def get_stored_username():
+	"""Get stored username if available"""
+	filename = 'username.json'
+	try:
+		with open(filename) as f:
+			username = json.load(f)
+	except FileNotFoundError:
+		return None 
+	else:
+		return username
+
+def get_new_username():
+	"""Prompt for a new username."""
+	username = input("What is your name? ")
+	filename = 'username.json'
+	with open (filename, 'w') as f:
+		json.dump(username, f)
+	return username
+
+
+def greet_user():
+	"""Greet the user by name"""
+	username = get_stored_username()
+	if username:
+		print(f"Welcome back, {username}")
+	else:
+		username = get_new_username()
+		print(f"We'll remember you when you come back, {username}.")
 
 
 
